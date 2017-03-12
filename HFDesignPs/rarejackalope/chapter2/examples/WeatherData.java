2e66c0e00566a877d957b2bb711250cfac13389b
RareJackalope <rarejackalope@gmail.com>
2017-03-12 15:28:40 -0500

    Initial commit looking at the observer pattern.

---
+++ HFDesignPs/rarejackalope/chapter2/examples/WeatherData.java
@@ -0,0 +1,26 @@
+package rarejackalope.chapter2.examples;
+
+public class WeatherData 
+{
+	public void getTemperature()
+	{
+		//gets temperature
+	}
+	
+	public void getHumidity()
+	{
+		//gets humidity
+	}
+	
+	public void getPressure()
+	{
+		//gets pressure
+	}
+	
+	public void measurementsChanged()
+	{
+		
+	}
+	
+	//Other methods below.
+}
