eedb6938c5f55bc8c59cea2eada5f3f02edaebd6
RareJackalope <rarejackalope@gmail.com>
2017-03-12 15:53:39 -0500

    Weatherdata with implementations for notifying, removing and adding observer objects.

--- HFDesignPs/rarejackalope/chapter2/examples/WeatherData.java
+++ HFDesignPs/rarejackalope/chapter2/examples/WeatherData.java
@@ -1,26 +1,60 @@
 package rarejackalope.chapter2.examples;
 
-public class WeatherData 
-{
-	public void getTemperature()
-	{
-		//gets temperature
-	}
+import java.util.ArrayList;
+
+public class WeatherData implements Subject
+{	
+	private ArrayList observers;
+	private float temperature;
+	private float humidity;
+	private float pressure;
 	
-	public void getHumidity()
+	public WeatherData()
 	{
-		//gets humidity
-	}
-	
-	public void getPressure()
-	{
-		//gets pressure
+		observers = new ArrayList();
 	}
 	
 	public void measurementsChanged()
 	{
+		notifyObserver();
+	}
+
+	@Override
+	public void registerObserver(Observer o) 
+	{
+		observers.add(o);
 		
 	}
+
+	@Override
+	public void removeObserver(Observer o) 
+	{
+		int i = observers.indexOf(o);
+		if(i >= 0)
+		{
+			observers.remove(i);
+		}
+		
+	}
+
+	@Override
+	public void notifyObserver() 
+	{
+		for(int i = 0; i < observers.size();i++)
+		{
+			Observer observer = (Observer)observers.get(i);
+			observer.update(temperature, humidity, pressure);
+		}
+	}
 	
-	//Other methods below.
+	public void setMeasurements(float temperature, float humidity, float pressure)
+	{
+		this.temperature = temperature;
+		this.pressure = pressure;
+		this.humidity = humidity;
+		measurementsChanged();
+	}
+	//Other Weatherdata methods below.
+		
 }
+	
