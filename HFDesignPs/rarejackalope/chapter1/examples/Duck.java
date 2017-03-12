ede9ae4463873cbfe961cac2e11b208d9a1d94df
RareJackalope <rarejackalope@gmail.com>
2017-03-11 19:00:42 -0600

    My implementation of the diagram from page 2 of hfdp

---
+++ HFDesignPs/rarejackalope/chapter1/examples/Duck.java
@@ -0,0 +1,17 @@
+package rarejackalope.chapter1.examples;
+
+public abstract class Duck 
+{
+	public static void quack()
+	{
+		System.out.println("Quack");
+	}
+	
+	public static void swim()
+	{
+		System.out.println("Much swim.");
+	}
+	
+	public abstract void display();
+
+}
