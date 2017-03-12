727300e49cd94076bf4abcc4bb4930120010b9da
RareJackalope <rarejackalope@gmail.com>
2017-03-11 19:02:24 -0600

    Extra classes extending the functionality of the Duck class.

---
+++ HFDesignPs/rarejackalope/chapter1/examples/MallardDuck.java
@@ -0,0 +1,11 @@
+package rarejackalope.chapter1.examples;
+
+public class MallardDuck extends Duck
+{
+
+	@Override
+	public void display() {
+		System.out.println("This duck appears Mallardy.");
+	}
+	
+}
---
+++ HFDesignPs/rarejackalope/chapter1/examples/RedheadDuck.java
@@ -0,0 +1,12 @@
+package rarejackalope.chapter1.examples;
+
+public class RedheadDuck extends Duck
+{
+
+	@Override
+	public void display() 
+	{
+		System.out.println("This duck has a bit of a redhead.");
+	}
+
+}
